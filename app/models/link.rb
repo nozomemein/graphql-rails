c88d6ef03648c61b3e7e52dd0dfb844e77a049f6
nozomemein <b8yukifsukeo999n@gmail.com>
2024-03-02 12:55:59 +0900

    refactor: add link validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,9 @@
 class Link < ApplicationRecord
   belongs_to :user, optional: true
 
+  validates :url, presence: true, length: {minimum: 5}
+  validates :description, presence: true, length: {minimum: 5}
+
   has_many :votes, dependent: :destroy
+
 end
